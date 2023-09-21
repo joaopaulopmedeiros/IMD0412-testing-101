cf2a74a1f4aebe0841e9cd9fa41b77de33869fae
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-09-21 01:54:08 -0300

    test: consolidação parcial

--- src/test/java/teste/MatriculaTest.java
+++ src/test/java/teste/MatriculaTest.java
@@ -22,6 +22,62 @@ class MatriculaTest {
 	}
 
 	@Test
+	void AprovadoPorNota() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(5));
+		m.registrarNota2(BigDecimal.valueOf(3));
+		m.registrarNota3(BigDecimal.valueOf(8));
+		m.registrarFrequencia(100);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.APRN, m.status());
+	}
+
+	@Test
+	void RecuperacaoPorNota() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(6));
+		m.registrarNota2(BigDecimal.valueOf(8));
+		m.registrarNota3(BigDecimal.valueOf(2));
+		m.registrarFrequencia(100);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REC, m.status());
+	}
+
+	@Test
+	void RecuperacaoPorMedia() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(4));
+		m.registrarNota2(BigDecimal.valueOf(5));
+		m.registrarNota3(BigDecimal.valueOf(4));
+		m.registrarFrequencia(100);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REC, m.status());
+	}	
+
+	@Test
+	void RecuperacaoPorMediaENota() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(2));
+		m.registrarNota2(BigDecimal.valueOf(3));
+		m.registrarNota3(BigDecimal.valueOf(4));
+		m.registrarFrequencia(100);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REC, m.status());
+	}		
+	
+	@Test
 	void Reprovado() {
 		Matricula m = new Matricula();
 		
@@ -33,5 +89,33 @@ class MatriculaTest {
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.REP, m.status());
+	}
+	
+	@Test
+	void ReprovadoPorFalta() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(10));
+		m.registrarNota2(BigDecimal.valueOf(9));
+		m.registrarNota3(BigDecimal.valueOf(9));
+		m.registrarFrequencia(25);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REPF, m.status());
+	}
+
+	@Test
+	void ReprovadoPorMediaEFalta() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(1));
+		m.registrarNota2(BigDecimal.valueOf(2));
+		m.registrarNota3(BigDecimal.valueOf(1));
+		m.registrarFrequencia(25);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REMF, m.status());
 	}
 }
