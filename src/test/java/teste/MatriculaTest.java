f2796bdedf758752f0d4955ec61c7bbe1066ed3c
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-09-21 10:41:07 -0300

    test: utilizar parameterized test

--- src/test/java/teste/MatriculaTest.java
+++ src/test/java/teste/MatriculaTest.java
@@ -3,116 +3,117 @@ package teste;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MatriculaTest {
 
-	@Test
-	void Aprovado() {
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"7,8,9,75",
+		"7,8,9,100"		
+	})
+	void Aprovado(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(7));
-		m.registrarNota2(BigDecimal.valueOf(8));
-		m.registrarNota3(BigDecimal.valueOf(9));
-		m.registrarFrequencia(100);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.APR, m.status());
 	}
 
-	@Test
-	void AprovadoPorNota() {
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"3,5,8,75",
+		"3,5,8,100"		
+	})
+	void AprovadoPorNota(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(5));
-		m.registrarNota2(BigDecimal.valueOf(3));
-		m.registrarNota3(BigDecimal.valueOf(8));
-		m.registrarFrequencia(100);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.APRN, m.status());
 	}
 
-	@Test
-	void RecuperacaoPorNota() {
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"4,5,4,75",
+		"6,8,2,75",	
+		"2,3,4,75",
+		"4,5,4,100",
+		"6,8,2,100",	
+		"2,3,4,100",			
+	})
+	void Recuperacao(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(6));
-		m.registrarNota2(BigDecimal.valueOf(8));
-		m.registrarNota3(BigDecimal.valueOf(2));
-		m.registrarFrequencia(100);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.REC, m.status());
 	}
-
-	@Test
-	void RecuperacaoPorMedia() {
+		
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"0,1,2,75",
+		"0,1,2,100"		
+	})
+	void Reprovado(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(4));
-		m.registrarNota2(BigDecimal.valueOf(5));
-		m.registrarNota3(BigDecimal.valueOf(4));
-		m.registrarFrequencia(100);
-		
-		m.consolidarParcialmente();
-		
-		Assertions.assertEquals(StatusAprovacao.REC, m.status());
-	}	
-
-	@Test
-	void RecuperacaoPorMediaENota() {
-		Matricula m = new Matricula();
-		
-		m.registrarNota1(BigDecimal.valueOf(2));
-		m.registrarNota2(BigDecimal.valueOf(3));
-		m.registrarNota3(BigDecimal.valueOf(4));
-		m.registrarFrequencia(100);
-		
-		m.consolidarParcialmente();
-		
-		Assertions.assertEquals(StatusAprovacao.REC, m.status());
-	}		
-	
-	@Test
-	void Reprovado() {
-		Matricula m = new Matricula();
-		
-		m.registrarNota1(BigDecimal.valueOf(1));
-		m.registrarNota2(BigDecimal.valueOf(2));
-		m.registrarNota3(BigDecimal.valueOf(1));
-		m.registrarFrequencia(100);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.REP, m.status());
 	}
-	
-	@Test
-	void ReprovadoPorFalta() {
+
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"10,9,8,0",
+		"10,9,8,74"		
+	})
+	void ReprovadoPorFalta(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(10));
-		m.registrarNota2(BigDecimal.valueOf(9));
-		m.registrarNota3(BigDecimal.valueOf(9));
-		m.registrarFrequencia(25);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
 		Assertions.assertEquals(StatusAprovacao.REPF, m.status());
 	}
 
-	@Test
-	void ReprovadoPorMediaEFalta() {
+	@ParameterizedTest
+	@CsvSource(delimiter = ',', value = {
+		"1,2,1,0",
+		"1,2,1,74"		
+	})
+	void ReprovadoPorMediaEFalta(Double nota1, Double nota2, Double nota3, Integer frequencia) {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(1));
-		m.registrarNota2(BigDecimal.valueOf(2));
-		m.registrarNota3(BigDecimal.valueOf(1));
-		m.registrarFrequencia(25);
+		m.registrarNota1(BigDecimal.valueOf(nota1));
+		m.registrarNota2(BigDecimal.valueOf(nota2));
+		m.registrarNota3(BigDecimal.valueOf(nota3));
+		m.registrarFrequencia(frequencia);
 		
 		m.consolidarParcialmente();
 		
