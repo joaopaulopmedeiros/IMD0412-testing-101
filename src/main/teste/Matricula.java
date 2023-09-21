1e4ca89c0c2713ef12fd7641a6f060580a488013
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-09-21 01:36:34 -0300

    feat: consolidação parcial de matrícula

--- src/main/teste/Matricula.java
+++ src/main/teste/Matricula.java
@@ -12,6 +12,8 @@ public class Matricula {
 	private BigDecimal nota2;
 
 	private BigDecimal nota3;
+
+	private BigDecimal mediaParcial;
 	
 	private Integer frequencia;
 
@@ -27,6 +29,10 @@ public class Matricula {
 
 	public BigDecimal nota3() {
 		return this.nota3;
+	}
+
+	public BigDecimal mediaParcial() {
+		return this.mediaParcial;
 	}
 
 	public StatusAprovacao status() {
@@ -45,13 +51,32 @@ public class Matricula {
 		this.nota3 = nota3;
 	}
 
-	/**
-	 * Segue as regras estabelecidas pelos artigos do regulamento de graduação da UFRN: http://www.sistemas.ufrn.br/download/sigaa/public/regulamento_dos_cursos_de_graduacao.pdf
-	 * 
-	 * A partir do artigo 104
-	 */
 	public void consolidarParcialmente() {
-		this.status = StatusAprovacao.APR;	
+		this.registrarMediaParcial();
+
+		if (this.estaAprovado()) 
+		{
+			this.status = StatusAprovacao.APR;
+		} 
+		else if (this.estaAprovadoPorNota()) 
+		{
+			this.status = StatusAprovacao.APRN;
+		}
+		else if (this.estaEmRecuperacao()) 
+		{
+			this.status = StatusAprovacao.REC;
+		} 
+		else if (this.estaReprovado()) 
+		{
+			this.status = StatusAprovacao.REP;
+		}
+		else if (this.estaReprovadoPorMediaEFalta()) 
+		{
+			this.status = StatusAprovacao.REMF;
+		}
+		else {
+			this.status = StatusAprovacao.REPF;	
+		}
 	}
 
 	public Integer frequencia() {
@@ -60,5 +85,63 @@ public class Matricula {
 
 	public void registrarFrequencia(Integer frequencia) {
 		this.frequencia = frequencia;
+	}
+
+	private void registrarMediaParcial() {
+		BigDecimal notaSum = this.nota1().add(this.nota2()).add(this.nota3());		
+        BigDecimal media = notaSum.divide(BigDecimal.valueOf(3), 2, BigDecimal.ROUND_HALF_UP);
+		this.mediaParcial = media;
+	}
+
+	/**
+	 * Média parcial igual ou superior a 7,0 com assiduidade (frequência) igual ou superior a 75% 
+	 */
+	private Boolean estaAprovado() {
+		return this.mediaParcial().compareTo(BigDecimal.valueOf(7)) >= 0 && this.frequencia() >= 75;
+	}
+
+	/**
+	 * Média parcial igual ou superior a 5,0 e menor que 7,0 com nota igual ou superior a 3,0 em todas as unidades
+	 */
+	private Boolean estaAprovadoPorNota() {
+		BigDecimal tres = BigDecimal.valueOf(3);
+		BigDecimal cinco = BigDecimal.valueOf(5);
+		BigDecimal sete = BigDecimal.valueOf(7);
+
+		return this.mediaParcial().compareTo(cinco) >= 0 &&
+		this.mediaParcial().compareTo(sete) < 0 &&
+		this.nota1().compareTo(tres)>= 0 && this.nota2().compareTo(tres)>= 0 && this.nota3().compareTo(tres) >= 0 &&
+ 		this.frequencia() >= 75;
+	}
+
+	/**
+	 * Recuperação por:
+	 * Nota: Média parcial igual ou superior a 5,0 e menor que 7,0 com nota inferior a 3,0 em pelo menos uma das unidades
+	 * Média: Média parcial igual ou superior a 3,0 e menor do que 5,0 com nota igual ou superior a 3,0 em todas as unidades
+	 * Média e Nota: Média parcial igual ou superior a 3,0 e menor do que 5,0 com nota inferior a 3,0 em pelo menos uma das unidades
+	 */
+	private Boolean estaEmRecuperacao() {
+		BigDecimal tres = BigDecimal.valueOf(3);
+		BigDecimal cinco = BigDecimal.valueOf(5);
+		BigDecimal sete = BigDecimal.valueOf(7);
+		
+		Boolean emRecuperacaoPorNota = this.mediaParcial().compareTo(cinco) >= 0 && this.mediaParcial().compareTo(sete) < 0 && (this.nota1().compareTo(tres) < 0 || this.nota2().compareTo(tres) < 0 || this.nota3().compareTo(tres) < 0);
+		Boolean emRecuperacaoPorMedia = this.mediaParcial().compareTo(tres) >= 0 && this.mediaParcial().compareTo(cinco) < 0 && (this.nota1().compareTo(tres) >= 0 && this.nota2().compareTo(tres) >= 0 && this.nota3().compareTo(tres) >= 0);
+		Boolean emRecuperacaoPorMediaENota = this.mediaParcial().compareTo(tres) >= 0 && this.mediaParcial().compareTo(cinco) < 0 && (this.nota1().compareTo(tres) < 0 || this.nota2().compareTo(tres) < 0 || this.nota3().compareTo(tres) < 0);
+		return (emRecuperacaoPorMedia || emRecuperacaoPorNota || emRecuperacaoPorMediaENota) && this.frequencia() >= 75;
+	}
+
+	/**
+	 * Média parcial inferior a 3,0 e assiduidade igual ou superior a 75%
+	 */
+	private Boolean estaReprovado() {
+		return this.mediaParcial().compareTo(BigDecimal.valueOf(3)) < 0 && this.frequencia() >= 75;
+	}
+
+	/**
+	 * Média parcial inferior a 3,0 e frequência inferior a 75%
+	 */
+	private Boolean estaReprovadoPorMediaEFalta() {
+		return this.mediaParcial().compareTo(BigDecimal.valueOf(3)) < 0 && this.frequencia() < 75;
 	}
 }
