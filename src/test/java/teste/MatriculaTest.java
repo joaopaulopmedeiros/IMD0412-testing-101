8247690d76d680a7d9d444b3618f58fa983da4ac
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-09-21 01:38:32 -0300

    refactor: remover código inutilizado

--- src/test/java/teste/MatriculaTest.java
+++ src/test/java/teste/MatriculaTest.java
@@ -1,7 +1,5 @@
 package teste;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,12 +8,12 @@ import org.junit.jupiter.api.Test;
 class MatriculaTest {
 
 	@Test
-	void test() {
+	void Aprovado() {
 		Matricula m = new Matricula();
 		
-		m.registrarNota1(BigDecimal.valueOf(10l));
-		m.registrarNota2(BigDecimal.valueOf(10l));
-		m.registrarNota3(BigDecimal.valueOf(10l));
+		m.registrarNota1(BigDecimal.valueOf(7));
+		m.registrarNota2(BigDecimal.valueOf(8));
+		m.registrarNota3(BigDecimal.valueOf(9));
 		m.registrarFrequencia(100);
 		
 		m.consolidarParcialmente();
@@ -23,4 +21,17 @@ class MatriculaTest {
 		Assertions.assertEquals(StatusAprovacao.APR, m.status());
 	}
 
+	@Test
+	void Reprovado() {
+		Matricula m = new Matricula();
+		
+		m.registrarNota1(BigDecimal.valueOf(1));
+		m.registrarNota2(BigDecimal.valueOf(2));
+		m.registrarNota3(BigDecimal.valueOf(1));
+		m.registrarFrequencia(100);
+		
+		m.consolidarParcialmente();
+		
+		Assertions.assertEquals(StatusAprovacao.REP, m.status());
+	}
 }
